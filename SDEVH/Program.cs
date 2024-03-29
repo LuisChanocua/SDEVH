a4a3d93eb798670be6e6a3a3e82a93d6fe22b1e7
LuisChanocua <esteban083057@gmail.com>
2024-03-29 00:08:29 -0600

    Algunas vistar enrutadas

--- SDEVH/Program.cs
+++ SDEVH/Program.cs
@@ -1,3 +1,4 @@
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -8,10 +9,13 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+//Errores perzonalidazados
+app.UseExceptionHandler("/Error");
+
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
@@ -20,8 +24,42 @@ app.UseRouting();
 
 app.UseAuthorization();
 
+
+#region RutasHome
+app.MapControllerRoute(
+        name: "ControlUsuarios",
+        pattern: "ControlUsuarios",
+        defaults: new { controller = "Home", action = "ControlUsuarios" });
+#endregion
+
+#region Errores
+app.MapControllerRoute(
+        name: "Error404",
+        pattern: "Error404",
+        defaults: new { controller = "Errores", action = "Error404" });
+
+app.MapControllerRoute(
+        name: "Error500",
+        pattern: "Error500",
+        defaults: new { controller = "Errores", action = "Error500" });
+
+
+
+#endregion
+
+
+#region Rutas Default
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+/*Si no encuentra la ruta lo manda a 404*/
+app.MapControllerRoute(
+            name: "Error404",
+            pattern: "{*url}",
+            defaults: new { controller = "Errores", action = "Error404" });
+
+#endregion
+
 
 app.Run();
